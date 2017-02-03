793eb2deb578fa4121f23bc56ac9046463e174cc
samuelpych <samuelpych@hanovernorwichschools.org>
2017-02-03 14:16:08 -0500

    Updated from Brython Server: 2/3/2017 2:16:08 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,22 +1,3 @@
-
-
-Write and submit a Python program that computes an approximate value of π by calculating the following sum:
-
-(see: https://github.com/HHS-IntroProgramming/Calculate-Pi/blob/master/README.md)
-
-This sum approaches the true value of π as n approaches ∞.
-
-Your program must ask the user how many terms to use in the estimate of π, how many decimal places, 
-then print the estimate using that many decimal places. Exactly like this:
-
-I will estimate pi. How many terms should I use? 100
-How many decimal places should I use in the result? 7
-The approximate value of pi is 3.1315929
-
-
-Note: remember that the printed value of pi will be an estimate!
-
-"""
 +Author: Sam
  +Credit: none
   Assignment:
@@ -26,9 +7,9 @@ Note: remember that the printed value of pi will be an estimate!
   Note: remember that the printed value of pi will be an estimate!
   
   """
- +import math
- +n = int(input("I will estimate pi. How many terms should I use? "))
- +decimals = int(input("How many decimal places should I use in the result? "))
- +pi = 1.0/sum([((-1.0)**k)/math.factorial(k) for k in range(0,n)])
- +print("The approximate value of pi is {0}".format(round(math.pi, decimals)))
- +print("(The true value of pi is {0})".format(round(math.pi, decimals)))
+import math
+n = int(input("I will estimate pi. How many terms should I use? "))
+decimals = int(input("How many decimal places should I use in the result? "))
+pi = 4*sum([((-1.0)**k)/((2*k+1)) for k in range(0,n)])
+print("The approximate value of pi is {0}".format(round(pi, decimals)))
+
